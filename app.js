a90463f0a7212b883a501f4aa9c9eeb1614f4663
Holly <holly_cooper@live.com.au>
2021-01-25 09:40:32 +1000

    style

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = () => {
+  const song = document.querySelector(.'song');
+  const play = document.querySelector(.'play');
+  const outline = document.querySelector(.'moving-outline circle');
+  const video = document.querySelector(.'vid-container video');
+
+//  Meditation  Sounds
+  const sounds = document.querySelectorAll('.sound-picker button');
+// Time Display
+  const timeDisplay = document.querySelector('.time-display');
+// Getting length of circle outline
+  const outlineLength = outline.getTotalLength();
+  console log(outlineLength)
+}
+
+
+
+
+
+
+
+app();
